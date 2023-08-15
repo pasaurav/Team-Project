b7239ff571c96e816707c159c6da1e63921e81dc
ramneek <sekhramn@sheridancollege.ca>
2023-08-14 21:25:17 -0400

    Group of card class added

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/GroupOfCards.java
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/GroupOfCards.java
@@ -6,7 +6,8 @@
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 /**
  * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
@@ -14,42 +15,46 @@ import java.util.Collections;
  *
  * @author dancye
  * @author Paul Bonenfant Jan 2020
+ * @modifier Ramneek kaur, (991710454)
+ * 
  */
-public class GroupOfCards {
+public class GroupOfCards 
+{
+    private List<Card> cards;
+    
+    public GroupOfCards() 
+    {
+    cards = new ArrayList<>();
+    String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+    int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
 
-    //The group of cards, stored in an ArrayList
-    private ArrayList<Card> cards;
-    private int size;//the size of the grouping
-
-    public GroupOfCards(int size) {
-        this.size = size;
+    for (String suit : suits) 
+    {
+      for (int i = 0; i < ranks.length; i++) {
+        Card card = new Card(suit, ranks[i], values[i]);
+        cards.add(card);
+      }
     }
+  }
 
-    /**
-     * A method that will get the group of cards as an ArrayList
-     *
-     * @return the group of cards.
-     */
-    public ArrayList<Card> getCards() {
-        return cards;
+  public void shuffle() 
+  {
+    Random random = new Random();
+    for (int i = cards.size() - 1; i > 0; i--) {
+      int j = random.nextInt(i + 1);
+      Card temp = cards.get(i);
+      cards.set(i, cards.get(j));
+      cards.set(j, temp);
     }
-
-    public void shuffle() {
-        Collections.shuffle(cards);
+  }
+  
+  public Card dealCard() 
+  {
+    if (cards.isEmpty()) 
+    {
+      throw new RuntimeException("Deck is empty!");
     }
-
-    /**
-     * @return the size of the group of cards
-     */
-    public int getSize() {
-        return size;
-    }
-
-    /**
-     * @param size the max size for the group of cards
-     */
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-}//end class
+    return cards.remove(cards.size() - 1);
+  }
+}
