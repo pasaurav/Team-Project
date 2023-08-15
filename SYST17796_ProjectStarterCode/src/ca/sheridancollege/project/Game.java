2968654692f1fc580cd829302173b61097afed75
kau15140 <134000692+kau15140@users.noreply.github.com>
2023-08-14 21:53:49 -0400

    Update Game.java

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Game.java
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Game.java
@@ -17,10 +17,9 @@ public class Game
  * @modifier Arshdeep Kaur, 991711763
  * 
  */
-public static void main(String[] args) 
-{
+public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter your name: ");
+    System.out.print("Please Enter your name: ");
     String playerName = scanner.nextLine();
     Player player = new Player(playerName);
     Player dealer = new Player("Dealer");
@@ -33,63 +32,54 @@ public static void main(String[] args)
     player.addCard(GroupOfCards.dealCard());
     dealer.addCard(GroupOfCards.dealCard());
 
-    System.out.println("----- Blackjack -----");
+    System.out.println("Blackjack");
     System.out.println("Welcome, " + player.getName() + "!");
     System.out.println("Let's play!");
 
     boolean gameOver = false;
     while (!gameOver) {
-      System.out.println("\n--- Player's Turn ---");
+      System.out.println("\n Now, It's Player's Turn ");
       player.playTurn(GroupOfCards, scanner);
 
-      if (player.isBust()) 
-      {
+      if (player.isBust()) {
         gameOver = true;
         break;
       }
 
-      System.out.println("\n--- Dealer's Turn ---");
+      System.out.println("\nNow, It's Dealer's Turn");
       dealer.playTurn(GroupOfCards);
 
-      if (dealer.isBust()) 
-      {
+      if (dealer.isBust()) {
         gameOver = true;
         break;
       }
 
-      if (player.isStaying() && dealer.isStaying()) 
-      {
+      if (player.isStaying() && dealer.isStaying()) {
         gameOver = true;
       }
     }
 
-    System.out.println("\n--- Game Over ---");
+    System.out.println("\n  The Game is Over");
     System.out.println("Player's Hand: " + player.getHand());
     System.out.println("Dealer's Hand: " + dealer.getHand());
 
-    if (player.isBust()) 
-    {
-      System.out.println("You went bust! Dealer wins.");
-    } else if (dealer.isBust()) 
-    {
-      System.out.println("Dealer went bust! You win.");
-    } else 
-    {
+    if (player.isBust()) {
+      System.out.println("Sadly, you failed! Dealer triumphs.");
+    } else if (dealer.isBust()) {
+      System.out.println("The dealer failed! You triumph.");
+    } else {
       int playerScore = player.calculateScore();
       int dealerScore = dealer.calculateScore();
 
       System.out.println("Player's Score: " + playerScore);
       System.out.println("Dealer's Score: " + dealerScore);
 
-      if (playerScore > dealerScore) 
-      {
+      if (playerScore > dealerScore) {
         System.out.println("Congratulations! You win!");
-      } else if (playerScore < dealerScore) 
-      {
-        System.out.println("Dealer wins!");
-      } else 
-      {
-        System.out.println("It's a tie!");
+      } else if (playerScore < dealerScore) {
+        System.out.println("Oops! you loose, Dealer wins!");
+      } else {
+        System.out.println("And...It's a tie!");
       }
     }
   }
